25ec9e3ddff25f37b37a958719d42e9396b32beb
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-02-07 16:59:34 +0100

    backport proper title to az#1

--- az1.rb
+++ az1.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 get %r[^/az1/?$] do
 	"""
 <html>
+  <head>
+    <title>az#1 kopi party</title>
+  </head>
 	<body bgcolor='#000'>
 		<p style='text-align:center; margin-top: 50px'>
 			<img src='accesszweckloss1.gif' />
